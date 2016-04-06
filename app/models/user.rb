b98d3c09c14d1543e764c7e3055f2907ea9dc994
slatersan1 <cdlee212@gmail.com>
2016-04-06 18:14:26 -0400

    new user model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true,
+    format: {
+      with: /.+\@.+\..+/,
+      message: "must have an @ symbol and period"
+    }
+  validates :auth_token, presence: true
+
+  has_many :posts
+
+  def ensure_auth_token
+    unless self.auth_token
+      self.auth_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(auth_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
+end
