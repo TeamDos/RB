e3ceab75918b900ed5e27a85555f7a34192c7ee3
Jennifer Ford <jenncford@gmail.com>
2016-04-07 17:25:24 -0400

    game data everything

---
+++ app/views/game_data/create.json.jbuilder
@@ -0,0 +1,3 @@
+# json.user @game_data.user, :username
+# json.image @game_data.image_url
+# json.caption @game_data.caption
---
+++ db/migrate/20160407211737_game.rb
@@ -0,0 +1,9 @@
+class CreateGameData < ActiveRecord::Migration
+  def change
+    create_table :game_data do |t|
+      t.attachment :image
+      t.string :caption
+
+      t.timestamps null: false
+  end
+end
