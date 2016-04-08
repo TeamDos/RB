9278d547dacc73deecd8d11e26891f607c0492a5
slatersan1 <cdlee212@gmail.com>
2016-04-08 15:20:34 -0400

    update signups

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -6,7 +6,8 @@ class SignupsController < ApplicationController
     @user.ensure_auth_token  
     if @user.save
       render json: { user: @user.as_json(only: [:id, :username, :password, :auth_token]) }, status: :created
-      
+    
+      User.login(username: username, password: password)
            
     else
       render json: { errors: @user.errors.full_messages },
