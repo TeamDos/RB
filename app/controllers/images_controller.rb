8be2c8c08eb822fb5e90382019c9f0a9e3e934f8
slatersan1 <cdlee212@gmail.com>
2016-04-09 20:09:26 -0500

    image handling

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,12 +2,12 @@ class ImagesController < ApplicationController
   before_action :authenticate!, only: [:create]
 
   def create
-    binding.pry
+    # binding.pry
     @image = current_user.images.create(img: params["img"], 
                                         caption: params["caption"])
     if @image.save
-    #   render json: {image: @image, url: @image.img.url }, status: :created
-      render "create.json.jbuilder", status: :created
+      render json: {image: @image, url: @image.img.url }, status: :created
+      # render "create.json.jbuilder", status: :created
     else
       render json: {errors: @image.errors.full_messages}, status: :unprocessable_entity
     end
