563884ada6af1115b760e1f49d2a90146c252a8b
slatersan1 <cdlee212@gmail.com>
2016-04-10 16:21:36 -0400

    modify index method

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -17,7 +17,7 @@ class ImagesController < ApplicationController
   def index
     @images = Image.all
     # @users = User.all
-    render "create.json.jbuilder", status: :ok
+    # render "create.json.jbuilder", status: :ok
   end
 
 end 
