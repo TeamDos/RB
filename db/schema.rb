2563b38a10400335db9a40c29f2c26106470f09f
slatersan1 <cdlee212@gmail.com>
2016-04-11 14:15:39 -0400

    change name of leaderboard controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411011038) do
+ActiveRecord::Schema.define(version: 20160411180244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160411011038) do
     t.string   "country"
   end
 
-  create_table "user_scores", force: :cascade do |t|
+  create_table "leaderboard", force: :cascade do |t|
     t.string  "username"
     t.integer "score"
   end
